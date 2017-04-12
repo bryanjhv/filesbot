26b2da6f439c98f36af959f8f4698c4a0e32a5e0
Bryan Horna <bryanjhv@gmail.com>
2017-04-11 19:13:32 -0500

    Notify server on finish

--- utils/notify.js
+++ utils/notify.js
@@ -1,10 +1,18 @@
+const request = require('request');
+
+
 /**
  * Notify download ready.
  *
  * @param {string} id User ID.
- * @param {string} name File name.
+ * @param {string} file File name.
  */
-module.exports = (id, name) => {
-  // TODO: Implement
-  console.log(id, name);
+module.exports = (id, file) => {
+  request({
+    method: 'PUT',
+    json: {id, file},
+    uri: process.env.SERVER_URL
+  }, (err, res, body) => {
+    // TODO: What to do here?
+  });
 };
